7305dd042477fa8fb3ed87cd622cee96ef5647bf
Vishnu-1003 <vishnumurukank2007@gmail.com>
2026-01-03 23:34:05 +0530

    Initial commit: Complete ROS2 Hello World assignment

--- src/hello_node.cpp
+++ src/hello_node.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -36,4 +37,30 @@ int main(int argc, char * argv[])
   rclcpp::spin(std::make_shared<HelloNode>());
   rclcpp::shutdown();
   return 0;
+=======
+#include "rclcpp/rclcpp.hpp"
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+class HelloNode : public rclcpp::Node {
+public:
+    HelloNode() : Node("hello_world_node") {
+        timer_ = this->create_wall_timer(
+            1000ms, std::bind(&HelloNode::timer_callback, this));
+    }
+
+private:
+    void timer_callback() {
+        RCLCPP_INFO(this->get_logger(), "Hello, World!");
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char ** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<HelloNode>());
+    rclcpp::shutdown();
+    return 0;
+>>>>>>> 0d99e0e (Initial commit: Complete ROS2 Hello World assignment)
 }
